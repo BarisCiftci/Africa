22fd9f8c84bc48ffb79a884b33e817c0a2d7a9ad
Baris Ciftci <barishciftci@gmail.com>
2022-10-23 17:55:31 +0200

    Set up the iOS project in Xcode, adding all assets, setting up colours, setting ups ap views and connect them to tabview

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Baris Ciftci on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
